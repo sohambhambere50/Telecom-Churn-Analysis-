0bb2b9fbe14c197683a149d7438d8c8ca8280ad3
Soham Bhambere <sohambhambere5050@gmail.com>
2025-06-05 19:07:05 +0530

    Update customerChurnAnalysis.py

--- customerChurnAnalysis.py
+++ customerChurnAnalysis.py
@@ -1,12 +1,1 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-df = pd.read_csv(r"C:\Users\Administrator\Desktop\Churn Analysis\Customer Churn.csv")
-# print(df.head())
-# print(df.info())
-df['TotalCharges'] = df['TotalCharges'].replace(" ","0")
-df['TotalCharges'] = df['TotalCharges'].astype("float")
-# print(df.info())
-print(df.isnull().sum())
